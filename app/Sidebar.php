cfc8e702090a933731c9fe1b71ddaa35ec739813
JiberBoom <JiberBoom@gmail.com>
2017-08-07 11:22:33 +0800

    修饰侧边栏，给链接加样式

--- app/Sidebar.php
+++ app/Sidebar.php
@@ -14,40 +14,45 @@ class Sidebar
     public function __construct()
     {
         Menu::make('MyNavBar', function($menu){
-//
-//            $menu->add('About',['url'=>'#','class'=>'tree-toggle nav-header'])
-//                 ->add('About-one',['url'=>'#','class'=>'tree-toggle nav-header'])
-//                 ->add('About-two',['url'=>'#','class'=>'tree-toggle nav-header'])
-//            ;
+
             //字体管理
-           $font =  $menu->add('字体管理',['url'=>'#','class'=>'glyphicon glyphicon-folder-open tree-toggle nav-header']);
+           $font =  $menu->add('字体管理',['url'=>'#','class'=>'tree-toggle nav-header']);
+           $font->link->attr(array('class'=>'glyphicon glyphicon-folder-open'));
+           $font->link->href('#');
 
-           $font->add('字体列表',['url'=>url('/admin/fonts/index'),'class'=>'tree-toggle nav-header']);
+           $font->add('字体列表',['url'=>url('/admin/fonts/index'),'class'=>'tree-toggle nav-header'])->link->attr(array('class'=>'glyphicon glyphicon-th-list'));
 
-           $font->add('美元价格列表',['url'=>'#','class'=>'tree-toggle nav-header']);
+           $font->add('美元价格列表',['url'=>'#','class'=>'tree-toggle nav-header'])->link->attr(array('class'=>'glyphicon glyphicon-th-list'));
 
-           $font->add('语种列表',['url'=>url('/admin/languages/index'),'class'=>'tree-toggle nav-header']);
+           $font->add('语种列表',['url'=>url('/admin/languages/index'),'class'=>'tree-toggle nav-header'])->link->attr(array('class'=>'glyphicon glyphicon-th-list'));
 
-           $font->add('模板图列表',['url'=>'#','class'=>'tree-toggle nav-header']);
+           $font->add('模板图列表',['url'=>'#','class'=>'tree-toggle nav-header'])->link->attr(array('class'=>'glyphicon glyphicon-th-list'));
 
 
             //壁纸管理
-           $wallpaper = $menu->add('壁纸管理',['url'=>'#','class'=>'glyphicon glyphicon-folder-open tree-toggle nav-header']);
+           $wallpaper = $menu->add('壁纸管理',['url'=>'#','class'=>'tree-toggle nav-header']);
 
-           $wallpaper->add('壁纸列表',['url'=>'#','class'=>'tree-toggle nav-header']);
+           $wallpaper->link->attr(array('class'=>'glyphicon glyphicon-folder-open'));
+           $wallpaper->link->href('#');
 
-           $wallpaper->add('壁纸类别列表',['url'=>'#','class'=>'tree-toggle nav-header']);
+           $wallpaper->add('壁纸列表',['url'=>'#','class'=>'tree-toggle nav-header'])->link->attr(array('class'=>'glyphicon glyphicon-th-list'));
+
+           $wallpaper->add('壁纸类别列表',['url'=>'#','class'=>'tree-toggle nav-header'])->link->attr(array('class'=>'glyphicon glyphicon-th-list'));
 
 
            //订单管理
-            $order = $menu->add('订单管理', ['url'=>'#','class'=>'glyphicon glyphicon-folder-open tree-toggle nav-header']);
+            $order = $menu->add('订单管理', ['url'=>'#','class'=>'tree-toggle nav-header']);
+            $order->link->attr(array('class'=>'glyphicon glyphicon-folder-open'));
+            $order->link->href('#');
 
-            $order->add('订单列表',['url'=>'#','class'=>'tree-toggle nav-header']);
+            $order->add('订单列表',['url'=>'#','class'=>'tree-toggle nav-header'])->link->attr(array('class'=>'glyphicon glyphicon-th-list'));
 
             //角色管理
-            $permission = $menu->add('角色权限管理', ['url'=>'#','class'=>'glyphicon glyphicon-folder-open tree-toggle nav-header']);
+            $permission = $menu->add('角色权限管理', ['url'=>'#','class'=>'tree-toggle nav-header']);
+            $permission->link->attr(array('class'=>'glyphicon glyphicon-folder-open'));
+            $permission->link->href('#');
 
-            $permission->add('角色列表',['url'=>url('/roles/index'),'class'=>'tree-toggle nav-header']);
+            $permission->add('角色列表',['url'=>url('/roles/index'),'class'=>'tree-toggle nav-header'])->link->attr(array('class'=>'glyphicon glyphicon-th-list'));;
 
         });
     }
